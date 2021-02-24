cfdd77cd6c75a589a2e90d2f5b809cb7085a3ad3
Rahul sajekar <53562276+rahulsajekar@users.noreply.github.com>
2021-02-24 12:45:29 +0530

    Create a method for calculate factorial of number

--- hello.java
+++ hello.java
@@ -1,5 +1,12 @@
 class hello{
     public static void public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println(calFact(5));
+    }
+    public static int calFact(int n){
+        if (n==1) return 1;
+        else{
+            return n*calFact(n-1);
+        }
     }
 }
