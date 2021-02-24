bdd92e873b750849c5e5640702a85ea1bb5de678
SajekarRahul <rahulsajekar93@gmail.com>
2021-02-24 12:53:52 +0530

    class signature changed

--- hello.java
+++ hello.java
@@ -1,5 +1,5 @@
 class hello{
-    public static void public static void main(String[] args) {
+     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println(calFact(5));
     }
